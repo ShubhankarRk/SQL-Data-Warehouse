39a2c3d1418354696f5f67fe0eba1633ebb704e7
Shubhankar Radhakrishna <59830898+ShubhankarRk@users.noreply.github.com>
2025-01-28 20:22:15 -0500

    ddl.sql

--- scripts/bronze/ddl.sql
+++ scripts/bronze/ddl.sql
@@ -8,72 +8,54 @@ Script Purpose:
 	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
-DROP TABLE IF EXISTS bronze.crm_cust_info;
 
-CREATE TABLE bronze.crm_cust_info (
-    cst_id              INT,
-    cst_key             VARCHAR(50),
-    cst_firstname       VARCHAR(50),
-    cst_lastname        VARCHAR(50),
-    cst_marital_status  VARCHAR(50),
-    cst_gndr            VARCHAR(50),
-    cst_create_date     DATE
-);
-
-DROP TABLE IF EXISTS bronze.crm_prd_info;
-
-CREATE TABLE bronze.crm_prd_info (
-    prd_id       INT,
-    prd_key      VARCHAR(50),
-    prd_nm       VARCHAR(50),
-    prd_cost     INT,
-    prd_line     VARCHAR(50),
-    prd_start_dt TIMESTAMP,
-    prd_end_dt   TIMESTAMP
-);
+DROP PROCEDURE IF EXISTS bronze.load_bronze;
 
 
-DROP TABLE IF EXISTS bronze.crm_sales_details;
+CREATE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    -- Load data into bronze.crm_cust_info
+    TRUNCATE TABLE bronze.crm_cust_info;
+    EXECUTE FORMAT(
+        'COPY bronze.crm_cust_info FROM %L WITH (FORMAT csv, HEADER true, DELIMITER '','')',
+        '/path/to/cust_info.csv'
+    );
 
+    -- Load data into bronze.crm_prd_info
+    TRUNCATE TABLE bronze.crm_prd_info;
+    EXECUTE FORMAT(
+        'COPY bronze.crm_prd_info FROM %L WITH (FORMAT csv, HEADER true, DELIMITER '','')',
+        '/path/to/prd_info.csv'
+    );
 
-CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num  VARCHAR(50),
-    sls_prd_key  VARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT
-);
+    -- Load data into bronze.crm_sales_details
+    TRUNCATE TABLE bronze.crm_sales_detials;
+    EXECUTE FORMAT(
+        'COPY bronze.crm_sales_detials FROM %L WITH (FORMAT csv, HEADER true, DELIMITER '','')',
+        '/path/to/sales_detials.csv'
+    );
 
-DROP TABLE IF EXISTS bronze.erp_loc_a101;
+    -- Load data into bronze.erp_cust_az12
+    TRUNCATE TABLE bronze.erp_cust_az12;
+    EXECUTE FORMAT(
+        'COPY bronze.erp_cust_az12 FROM %L WITH (FORMAT csv, HEADER true, DELIMITER '','')',
+        '/path/to/erp_cust_az12.csv'
+    );
 
+    -- Load data into bronze.erp_loc_a101
+    TRUNCATE TABLE bronze.erp_loc_a101;
+    EXECUTE FORMAT(
+        'COPY bronze.erp_loc_a101 FROM %L WITH (FORMAT csv, HEADER true, DELIMITER '','')',
+        '/path/to/erp_loc_a101.csv'
+    );
 
-CREATE TABLE bronze.erp_loc_a101 (
-    cid    VARCHAR(50),
-    cntry  VARCHAR(50)
-);
-
-
-
-DROP TABLE IF EXISTS bronze.erp_cust_az12;
-
-
-CREATE TABLE bronze.erp_cust_az12 (
-    cid    VARCHAR(50),
-    bdate  DATE,
-    gen    VARCHAR(50)
-);
-
-
-DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
-
-
-CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id           VARCHAR(50),
-    cat          VARCHAR(50),
-    subcat       VARCHAR(50),
-    maintenance  VARCHAR(50)
-);
+    -- Load data into bronze.erp_px_cat_g1v2
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+    EXECUTE FORMAT(
+        'COPY bronze.erp_px_cat_g1v2 FROM %L WITH (FORMAT csv, HEADER true, DELIMITER '','')',
+        '/path/to/erp_px_cat_g1v2.csv'
+    );
+END;
+$$;
